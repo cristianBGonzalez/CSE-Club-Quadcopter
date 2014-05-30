468541d31ee527fe2a29daeb005df36b1099c46d
Eli Gonzalez <egonzalezjjr55@gmail.com>
2014-05-30 15:31:46 -0700

    Serial Works but data read is corrupted

--- Serial.py
+++ Serial.py
@@ -1,10 +1,11 @@
 import wiringpi2
-wiringpi2.wiringPiSetupGpio()
+	#wiringpi2.wiringPiSetupGpio()
 serial = wiringpi2.serialOpen('/dev/ttyAMA0',9600) # Requires device/baud and returns an ID
+if serial == -1:
+	print('error')
 wiringpi2.delay(1000)
-while true:
+while 1:
 	#wiringpi2.serialPuts(serial,"hello")
-	wiringpi2.delay(1)
-	print(wiringpi2.serialGetchar (serial))
+	print(wiringpi2.serialGetchar(serial))
 
-wiringpi2.serialClose(serial) // Pass in ID
+wiringpi2.serialClose(serial)
